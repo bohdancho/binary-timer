0c001ce7f92c8fec2443e63f942084a79a892297
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-08-25 17:38:30 +0200

    setup app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # binary-timer
+
 Simple real-time clock representation of the binary number system for educational purposes
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.tw.css'
+
+function App() {
+  return (
+    <main className='flex h-screen w-screen items-center justify-center bg-slate-800 text-7xl text-white'>
+      App works!
+    </main>
+  )
+}
+
+export default App
